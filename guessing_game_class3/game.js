b78b0985d645cfae923f32592d9f06efa95f0309
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-04-09 15:06:35 -0700

    add counter variable and counter after each IF.

--- guessing_game_class3/game.js
+++ guessing_game_class3/game.js
@@ -1,15 +1,17 @@
 var dessert = "ICE CREAM"; // question one
 var team = "SEAHAWKS"; // question two
 var child = "SON"; // question three
+var counter = 0;
 
 var userName = prompt ("Hey, what's your name?"); //greeting
-alert ("Hey " + userName + " nice to meet you. Time to guess my favorite things");
+alert ("Hey " + userName + " nice to meet you. Time to guess some things about me. Let's see how many you get right.");
 
 var dessertAnswer = prompt ("Is my favorite dessert ice cream or cookies") //question one
 dessertAnswer = dessertAnswer.toUpperCase();
 console.log("user input for dessertAnswer " + dessertAnswer);
 if (dessert === dessertAnswer){
   alert ("Yep! I love ice cream")
+  counter++;
 } else { alert ("Actually, it's ice cream")}
 
 var teamAnswer = prompt ("Do I like the Seahawks or Mariners more?")//question two
@@ -17,6 +19,7 @@ teamAnswer = teamAnswer.toUpperCase();
 console.log("user input for teamAnswer " + teamAnswer);
 if (team === teamAnswer){
   alert ("That's right. Go hawks!")
+  counter++;
 } else {
   alert ("No. I'm more of a football fan.")
 }
@@ -26,6 +29,7 @@ childAnswer = childAnswer.toUpperCase();
 console.log("user input for childAnswer " + childAnswer);
 if (child === childAnswer){
   alert ("That's right. His name is Silas")
+  counter++;
 } else {
   alert ("Actually, I have a son.")
 }
@@ -37,9 +41,10 @@ guessAge = 3;
 var guessAge = parseInt(prompt ("How old do you think my son is?"))
 if (guessAge === 3){
   alert("That's right! You guessed it.")
+  counter++; 
 } else if (guessAge > 3) {
   alert("Too high. He's 3")
-  counter++;
 } else {alert("Close, he's 3")
   counter++;
 }
+alert("You got " + counter + " questions right!")
